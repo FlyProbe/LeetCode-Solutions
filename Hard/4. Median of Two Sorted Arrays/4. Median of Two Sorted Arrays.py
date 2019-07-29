056a70318755b4b13d11f9366a1725e592326e9d
FlyProbe <45419403+FlyProbe@users.noreply.github.com>
2019-07-29 15:16:18 +0800

    Update 4. Median of Two Sorted Arrays.py

--- Hard/4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.py
+++ Hard/4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.py
@@ -23,9 +23,11 @@ class Solution:
                 if i == 0: max_of_left = B[j-1]
                 elif j == 0: max_of_left = A[i-1]
                 else: max_of_left = max(A[i-1], B[j-1])
-
+                    
+                # 这里非常巧妙的避开了A=[], B=[1]的判空问题，如果把奇偶放在一起return，下面会报列表越界
+                # A=[], B=[1]时， imin = 0, imax = 0, half = 1, i = 0, j = 1, 这是i==m True, B[1]越界。
                 if (m + n) % 2 == 1:
-                    return max_of_left
+                    return max_of_left  
 
                 if i == m: min_of_right = B[j]
                 elif j == n: min_of_right = A[i]
